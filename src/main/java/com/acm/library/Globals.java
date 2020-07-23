d995704f15ce25c42e064f774d963b2af7bfb06a
Samuel Butler <sambutler1017@icloud.com>
2020-07-23 13:09:07 -0400

    Updating build.bradle

---
+++ src/main/java/com/acm/library/Globals.java
@@ -0,0 +1,7 @@
+package com.acm.library;
+
+public class Globals {
+	public static final String PROD_ENV = "/app/src/main";
+	public static final String DEV_ENV = "/app/src/main";
+	public static final String LOCAL_ENV = "../acm-app-microservice/src/main";
+}
