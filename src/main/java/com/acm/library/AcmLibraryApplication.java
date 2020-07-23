c17872eae9679f23a594c86fbf40cb38a26c9fd7
Samuel Butler <sambutler1017@icloud.com>
2020-07-23 13:17:32 -0400

    Updating gradle wrapper

---
+++ src/main/java/com/acm/library/AcmLibraryApplication.java
@@ -0,0 +1,12 @@
+package com.acm.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcmLibraryApplication.class, args);
+	}
+}
